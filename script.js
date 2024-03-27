fe762cc74dfbcd6a04049bf362172589495f0dfa
azizqani <nurmuxamedovumar@gmail.com>
2024-03-27 10:30:07 +0500

    home work

---
+++ script.js
@@ -0,0 +1,7 @@
+const arrayName = ["Olma" , "Banan" , "Gilos" , "Shaftoli"]
+console.log(arrayName);
+alert("Boshlanishda arrayni uzunligi " + arrayName.length)
+alert("Sizning arrayingizdan ma'lumotlarni olib tashlamoqshimiz")
+let arrayName2 = arrayName.pop( - 1)
+alert(arrayName.length)
+console.log(arrayName);
